6e69e85fe0620363dedb435518acdca12ec17076
Virgil <48660629+Virgil07@users.noreply.github.com>
2019-03-30 19:21:03 +0100

    Add files via upload

---
+++ tic.py
@@ -0,0 +1,44 @@
+a = 1
+b = 2
+c = 3
+d = 4
+e = 5
+f = 6
+g = 7
+h = 8
+i = 9
+
+
+def board():
+    print("  ...   ...   ...")
+    print("| ",a," | ",b," | ",c," |")
+    print("  ...   ...   ...")
+    print("| ",d," | ",e," | ",f," |")
+    print("  ...   ...   ...")
+    print("| ",g," | ",h," | ",i," |")
+    print("  ...   ...   ...")
+
+board()
+while True:
+    prompt1 = input("player 1 turn: ")
+
+
+    if prompt1 == 1:
+        a = x
+        board()
+        print("win")
+        prompt2= input("player 2 turn: ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
